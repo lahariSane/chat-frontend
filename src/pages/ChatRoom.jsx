bf64a9e63345c2e6bb2188b1c512fde39113e95c
Lahari <laharisane1511@gmail.com>
2025-01-24 01:22:28 +0530

    Revert 'User-Agent' header to 'ngrok-skip-browser-warning' in API requests in ChatRoom component

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -36,7 +36,7 @@ const ChatRoom = () => {
                     {
                         headers: {
                             Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            "User-Agent": "lahari",
+                            "ngrok-skip-browser-warning": "true",
                         },
                     }
                 );
